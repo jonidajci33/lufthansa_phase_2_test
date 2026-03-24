b8c691aeca81aab3f4b3ee55b3236e03ae936bcf
Joni Dajci <joni.dajci12@gmail.com>
2026-03-24 14:08:24 +0100

    test containers fix

--- notification-service/src/test/java/com/planningpoker/notification/infrastructure/client/RoomServiceClientTest.java
+++ notification-service/src/test/java/com/planningpoker/notification/infrastructure/client/RoomServiceClientTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.web.client.RestClient;
@@ -22,6 +24,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class RoomServiceClientTest {
 
     @Mock
